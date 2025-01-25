d775ea44834b939e50811d9d28ebe05dd71c2d82
Your Name <you@example.com>
2025-01-25 21:01:29 +0300

    was add pharmacy package

---
+++ pharmacy/main.py
@@ -0,0 +1,35 @@
+import threading
+import time
+
+
+class Pharmacy:
+    def __init__(self):
+        self.costumers = threading.Semaphore(value= 2)
+
+
+    def meet_costumer(self, costumers):
+
+        print(f"\nПриглашаем поситителя - {costumers}")
+        self.costumers.acquire()
+
+        print(f"\nРабота с поситителем - {costumers}")
+        time.sleep(5)
+
+        print(f"\nЗавершение работы с поситителем - {costumers}")
+        self.costumers.release()
+
+
+
+    def costumer(self, count):
+        for visitor in range(count):
+            costumer_thread = threading.Thread(target= self.meet_costumer,
+                                               args=(visitor,))
+            costumer_thread.start()
+
+
+if __name__ == "__main__":
+    pharmacy = Pharmacy()
+
+    count = 6
+    pharmacy.costumer(count)
+
